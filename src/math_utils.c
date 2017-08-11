52410c3a7d92240ef8e16c34b7e08da435b0862a
gndowns <gabriel.downs@shopify.com>
2017-08-11 16:48:27 -0400

    calculate roots

--- src/math_utils.c
+++ src/math_utils.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 
 typedef struct Quadratic {
   int a;
@@ -6,34 +7,48 @@ typedef struct Quadratic {
   int c;
 } Quadratic;
 
-int get_roots(Quadratic, float *);
+int get_roots(Quadratic, double *);
 void print(Quadratic);
 
 int main() {
 
   Quadratic quad = {1, 1, 1};
 
+  double roots[2];
+  // example with imaginary roots
+  int res = get_roots(quad, roots);
+  printf("calculating roots for ");
   print(quad);
+  printf("\n%d\n", res);
+
   printf("\n");
 
-  float roots[2];
-  // example with imaginary roots
-  Quadratic imaginary_roots = {1, 4, 5};
-  int res = get_roots(quad, roots);
-  printf("%d\n", res);
+  // example with real roots
+  Quadratic real_roots = {1, 8, 16};
+  printf("calculating roots for ");
+  print(real_roots);
+  res = get_roots(real_roots, roots);
+  printf("\n%d\n", res);
+  printf("first root: %f\n", roots[0]);
+  printf("second root: %f\n", roots[1]);
 
   return 0;
 }
 
-int get_roots(Quadratic q, float *roots) {
+int get_roots(Quadratic q, double *roots) {
   // quadratic formula
   // check if part under the radical is real
   float radicand = q.b * q.b - (4 * q.a * q.c);
   if (radicand < 0) {
     return -1;
   }
-  /* r1 = -q.b + square_root(q.b * q.b - 4 * q.a * q.c) / (2 * q.a); */
-  /* r2 = -q.b - square_root(q.b * q.b - 4 * q.a * q.c) / (2 * q.a); */
+
+  double r1 = ( -q.b + sqrt( radicand ) ) / (2 * q.a);
+  roots[0] = r1;
+
+  double r2 = ( -q.b - sqrt( radicand ) ) / (2 * q.a);
+  roots[1] = r2;
+
   return 0;
 }
 
