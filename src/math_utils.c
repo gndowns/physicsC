d786b737dd9a888f849d13161a40dce7b6761b6e
gndowns <gabriel.downs@shopify.com>
2017-08-12 01:54:49 -0400

    handle linear equations

--- src/math_utils.c
+++ src/math_utils.c
@@ -13,7 +13,20 @@ Quadratic distance_quadratic(Quadratic q1, Quadratic q2) {
 }
 
 int calculate_roots(Quadratic q, double *roots) {
-  // quadratic formula
+  // check if any solution
+  if (q.a == 0 && q.b == 0) {
+    return -1;
+  }
+  // check if linear equation
+  else if (q.a == 0) {
+    int res = -q.c / q.b;
+    roots[0] = res;
+    roots[1] = res;
+
+    return 0;
+  }
+
+  // else, quadratic formula
   // check if part under the radical is real
   float radicand = q.b * q.b - (4 * q.a * q.c);
   if (radicand < 0) {
--- src/test/test_math_utils.c
+++ src/test/test_math_utils.c
@@ -5,30 +5,54 @@
 int compare_quads(Quadratic, Quadratic);
 
 int main() {
-  Quadratic q1 = {1, 8, 16};
-  Quadratic q2 = {1, 5, 6};
 
-  printf("calculating distance between:\n");
-  print_quadratic(q1);
-  printf("\n");
-  print_quadratic(q2);
+  // test for linear equations
+  Quadratic linear = {0, 3, 9};
+  printf("calculating roots for: ");
+  print_quadratic(linear);
+  printf("\n\n");
+  printf("expected: ");
+  printf("root 1: -3, root 2: -3");
   printf("\n");
 
-  Quadratic q = distance_quadratic(q1, q2);
-
-  Quadratic expected = {0, 3, 10};
-  printf("\nexpected: ");
-  print_quadratic(expected);
-  printf("\n");
+  double roots[2];
+  int res = calculate_roots(linear, roots);
   printf("result: ");
-  print_quadratic(q);
+  printf("root 1: %f, root 2: %f", roots[0], roots[1]);
   printf("\n");
-  int comp = compare_quads(q, expected);
-  if (comp == 0) {
+  if (roots[0] == -3 && roots[1] == -3) {
     printf("SUCCESS\n");
   }
   else {
     printf("FAILURE\n");
+  }
+
+  if (0) {
+    Quadratic q1 = {1, 8, 16};
+    Quadratic q2 = {1, 5, 6};
+
+    printf("calculating distance between:\n");
+    print_quadratic(q1);
+    printf("\n");
+    print_quadratic(q2);
+    printf("\n");
+
+    Quadratic q = distance_quadratic(q1, q2);
+
+    Quadratic expected = {0, 3, 10};
+    printf("\nexpected: ");
+    print_quadratic(expected);
+    printf("\n");
+    printf("result: ");
+    print_quadratic(q);
+    printf("\n");
+    int comp = compare_quads(q, expected);
+    if (comp == 0) {
+      printf("SUCCESS\n");
+    }
+    else {
+      printf("FAILURE\n");
+    }
   }
 
   return 0;
