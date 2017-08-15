3f4a9ec9e53d8129f245b397458c420fd70d6d06
gndowns <gabriel.downs@shopify.com>
2017-08-14 23:01:07 -0400

    remove vestigial setup functions

--- src/main.c
+++ src/main.c
@@ -46,35 +46,3 @@ int main(int argc, char **argv) {
 
   return 0;
 }
-
-void create_particles(Cell *space)
-{
-  int numParticles;
-  printf("How many particles would you like to create? (0-2): ");
-  scanf("%d", &numParticles);
-
-  for (int i=0; i < numParticles; i++) {
-    printf("\nParticle %d\n", i+1);
-    int x = get_position(i+1);
-    space[x].on = 1;
-    space[x].v = get_attribute("velocity");
-    space[x].a = get_attribute("acceleration");
-    printf("\n");
-  }
-}
-
-int get_position()
-{
-  int x;
-  printf("Enter position (0-%d): ", SIZE - 1);
-  scanf("%d", &x);
-  return x;
-}
-
-int get_attribute(char *attr)
-{
-  int out;
-  printf("Enter %s: ", attr);
-  scanf("%d", &out);
-  return out;
-}
